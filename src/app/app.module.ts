aafe75375bf00789bc8b068001ef53006e1877a5
Raymond Rowland <raymond.rowland1092@gmail.com>
2019-02-19 13:07:46 -0400

    Primer Commit del homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './template/Home-page/navbar/navbar.component';
+import { CarouselComponent } from './template/Home-page/carousel/carousel.component';
+import { FormsComponent } from './template/Home-page/forms/forms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
