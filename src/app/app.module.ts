1b47ac145b3606af70b1d37e685b26e2f280c79a
Kevin Reyes <36552064+kfreyes@users.noreply.github.com>
2019-03-02 12:27:56 -0400

    Vistas login y registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './template/Home-page/navbar/navbar.component';
 import { CarouselComponent } from './template/Home-page/carousel/carousel.component';
 import { FormsComponent } from './template/Home-page/forms/forms.component';
-import { IngresarComponent } from './template/login/ingresar/ingresar.component';
+import { IngresarComponent } from './template/Login/ingresar/ingresar.component';
+import { RegistrarComponent } from './template/Registrarse/registrar/registrar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CarouselComponent,
-    FormsComponent
+    FormsComponent,
+    IngresarComponent,
+    RegistrarComponent
   ],
   imports: [
     BrowserModule,
